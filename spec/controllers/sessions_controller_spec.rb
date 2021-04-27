329b865ad3edfeed1c34bd4b3c3e23c00a25feb2
Jacob Ditkoff <jditkof1@binghamton.edu>
2021-04-27 19:59:26 +0000

    6.9 part 2

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  
   describe "#create" do
+    
+    context "no active session, User and Authorization already exist" do
+      context "Login with github" do
+        before(:each) do
+          session[:user_id] = nil
+          @user = User.create!(name: 'SUNY Tester', email: 'stester@binghamton.edu')
+          @auth = Authorization.create!(provider: "github", uid: "123456", user_id: @user.id)
+        end 
+        describe 'When logging in a registered user' do
+          
+          let(:id1)  {1}
+          let(:user_id1) {1}
+          let(:auth1) {@auth}
+          let(:user1) {@user}
+          
+          it "checks to see if a previous authorization exists" do
+          end
+          it 'recovers the previous authorization' do
+          end
+          it 'recovers the previous user' do
+          end
+          it 'sets the session' do
+          end
+          it 'sets the current user' do
+          end
+          it 'sets a flash message' do
+          end
+          it 'redirects to the home page' do
+          end
+        end
+      end
+    end
+    
     context "no active session, User and Authorization do not exist" do
       context 'register with github' do
         before do
@@ -14,6 +48,9 @@ RSpec.describe SessionsController, type: :controller do
           let(:user_id) {1}
           let(:user) {double('User', name: 'SUNY Tester', email: 'stester@binghamton.edu', id: id1)}
           let(:auth) {double('Authorization', provider: "github", uid: "123456", user_id: id1, user: double('User', name: 'SUNY Tester', email: 'stester@binghamton.edu', id: id1))}    
+          it 'checks to see that a previous authorization does not exist' do
+            post :create, provider: :github
+          end  
           it "creates a User" do
             expect { post :create, provider: :github }.to change(User, :count).by(1)
           end        
